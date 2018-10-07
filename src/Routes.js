a9bc0c32ec02cf7a30403b8d47a08521c0f01830
Gokhan Maden <gokhanmaden1@gmail.com>
2018-10-07 20:15:33 +0300

    Home components is done

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from "./Hoc/Layout";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 const Routes = (props) => {
   return(
     <Layout>
-      Routesdan gelen içerik
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
     </Layout>
   )
 }

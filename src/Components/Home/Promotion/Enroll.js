3468edf54c3dbf93f9ec9f02c3dc0ba832c02bae
gokhan.maden <gokhan.maden@bilyoner.com>
2018-10-11 13:33:36 +0300

    field validation development

--- src/Components/Home/Promotion/Enroll.js
+++ src/Components/Home/Promotion/Enroll.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from "react-reveal/Fade";
 import FormField from "../../UI/FormFields";
+import { validate } from "../../UI/Misc";
 
 class Enroll extends Component {
 
@@ -12,7 +13,7 @@ class Enroll extends Component {
       formSuccess: "",
       formdata: {
         email: {
-          element:"input-text",
+          element:"input",
           value:"",
           config:{
             name:"email_input",
@@ -29,6 +30,29 @@ class Enroll extends Component {
       }
     }
   }
+
+  updateForm(element) {
+    const newFormdata = {...this.state.formdata};
+    const newElement = { ...newFormdata[element.id]};
+
+    newElement.value = element.event.target.value;
+
+    let validData = validate(newElement);
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+
+    newFormdata[element.id] = newElement;
+    
+    this.setState({
+      formdata: newFormdata
+    });
+
+  }
+
+  submitForm() {
+
+  }
+
   render() {
     return (
       <Fade>
@@ -38,7 +62,11 @@ class Enroll extends Component {
               Enter your email
             </div>
             <div className="enroll_input">
-              FormField
+              <FormField 
+                id="email"
+                formdata={this.state.formdata.email}
+                change={(element) => this.updateForm(element)}
+              />
             </div>
           </form>
         </div>

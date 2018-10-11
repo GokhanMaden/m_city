18feb17dac42e88dbac28ce4a856771a52738f75
Gokhan Maden <gokhanmaden1@gmail.com>
2018-10-11 23:17:35 +0300

    dashboard component completed

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,15 @@ import Layout from "./Hoc/Layout";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./Components/Home";
-import SignIn from "./Components/SignIn"
+import SignIn from "./Components/SignIn";
+
+import Dashboard from "./Components/Admin/Dashboard";
 
 const Routes = (props) => {
   return(
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>

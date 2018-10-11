efd384600a9e9a9d84f9c5f4f33607a26123e4b3
gokhan.maden <gokhan.maden@bilyoner.com>
2018-10-11 18:10:55 +0300

    Last section of field

--- src/Components/UI/Misc.js
+++ src/Components/UI/Misc.js
@@ -51,6 +51,12 @@ export const reverseArray = (actualArray) => {
 export const validate = (element) => {
   let error = [true, ""];
 
+  if(element.validation.email) {
+    const valid = /\S+@\S+\.\S+/.test(element.value);
+    const message = `${!valid ? "Must be a valid email" : ""}`;
+    error = !valid ? [valid, message] : error;
+  }
+
   if(element.validation.required === true) {
     const valid = element.value.trim() !== "";
     const message = `${!valid ? "This fields is required" : ""}`;

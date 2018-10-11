3f6f6680584a7eecc0d91220071c3c8b64666926
Gokhan Maden <gokhanmaden1@gmail.com>
2018-10-11 21:44:00 +0300

    sign_in component

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from "./Hoc/Layout";
 import { Switch, Route } from "react-router-dom";
+
 import Home from "./Components/Home";
+import SignIn from "./Components/SignIn"
 
 const Routes = (props) => {
   return(
     <Layout>
       <Switch>
+        <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
